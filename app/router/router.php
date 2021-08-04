dc0cdbf28f4048b3aabffabf197d9d87f553371c
alexandrepalmag <alepalmag85ti@gmail.com>
2021-08-04 07:24:11 -0300

    Funtions to get dinamic routes

--- app/router/router.php
+++ app/router/router.php
@@ -10,10 +10,56 @@ function exactMatchUriInArrayRoutes($uri, $routes)
 
     if (array_key_exists($uri, $routes)) {
 
-        return [];
+        return [$uri => $routes[$uri]];
     }
 
     return [];
+}
+
+function regularExpressionMatchArrayRoutes($uri, $routes)
+{
+
+    return array_filter(
+
+        $routes,
+        function ($value) use ($uri) {
+
+            $regex = str_replace('/', '\/', ltrim($value, '/'));
+            return preg_match("/^$regex$/", ltrim($uri, '/'));
+        },
+
+        ARRAY_FILTER_USE_KEY
+    );
+}
+
+function params($uri, $matchedUri)
+{
+    if (!empty($matchedUri)) {
+
+        $matchedToGetParams = array_keys($matchedUri)[0];
+
+        return array_diff(
+            explode('/', ltrim($uri, '/')),
+            explode('/', ltrim($matchedToGetParams, '/'))
+        );
+    }
+
+    return [];
+}
+
+function paramsFormat($uri, $params)
+{
+
+    $uri = explode('/', ltrim($uri, '/'));
+
+    $paramsData = [];
+
+    foreach ($params as $index => $param) {
+
+        $paramsData[$uri[$index - 1]] = $param;
+    }
+
+    return $paramsData;
 }
 
 function router()
@@ -23,4 +69,18 @@ function router()
     $routes = routes();
 
     $matchedUri = exactMatchUriInArrayRoutes($uri, $routes);
+
+    if (empty($matchedUri)) {
+
+        $matchedUri = regularExpressionMatchArrayRoutes($uri, $routes);
+
+        if (!empty($matchedUri)) {
+
+            $params = params($uri, $matchedUri);
+            $params = params($uri, $params);
+
+
+            var_dump($params);
+        }
+    }
 }
--- app/router/routes.php
+++ app/router/routes.php
@@ -2,5 +2,7 @@
 
 return [
     '/' => 'Home@index',
-    '/user/create' => 'User@create'
+    '/user/create' => 'User@create',
+    '/user/[0-9]+' => 'User@index',
+    '/user/[0-9]+/name/[a-z]+' => 'User@show',
 ];
